522862e3605811c8cee27e76459a970449a94565
astryx <robertasdere@gmail.com>
2019-02-03 10:56:15 +0000

    Added sound

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+    public static AudioClip deathSound;
+    static AudioSource audiosrc;
+
+	// Use this for initialization
+	void Start () {
+        deathSound = Resources.Load<AudioClip>("neck");
+        audiosrc = GetComponent<AudioSource>();
+	}
+	
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case "death":
+                audiosrc.PlayOneShot(deathSound);
+                break;
+            default:
+                break;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
